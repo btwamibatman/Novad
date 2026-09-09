f4f65a9171b9f2e929782fb388e56702961c074e
btwamibatman <abdygalimhamza@gmail.com>
2026-09-09 16:23:45 +0500

    feat: enhance localization support by adding shared and tools refinement messages

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -2,6 +2,10 @@ import { createI18n } from 'vue-i18n'
 
 import en from './en.json'
 import ru from './ru.json'
+import sharedRefinementEn from './shared-refinement.en.json'
+import sharedRefinementRu from './shared-refinement.ru.json'
+import toolsRefinementEn from './tools-refinement.en.json'
+import toolsRefinementRu from './tools-refinement.ru.json'
 
 export type AppLocale = 'en' | 'ru'
 
@@ -16,5 +20,8 @@ export const i18n = createI18n({
   legacy: false,
   locale: initialLocale(),
   fallbackLocale: 'en',
-  messages: { en, ru },
+  messages: {
+    en: { ...en, ...sharedRefinementEn, ...toolsRefinementEn },
+    ru: { ...ru, ...sharedRefinementRu, ...toolsRefinementRu },
+  },
 })
